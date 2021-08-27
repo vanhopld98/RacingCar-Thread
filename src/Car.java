df4a27ebf596539d8c18b5cde056a52a2353fba0
vanhopld98 <vanhopld98@gmail.com>
2021-08-27 13:54:32 +0700

    done

---
+++ src/Car.java
@@ -0,0 +1,57 @@
+import java.util.Random;
+
+public class Car implements Runnable {
+    public static int DISTANCE = 100;
+    public static int STEP = 5;
+    private String name;
+
+    public Car(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public void run() {
+        int runDistance = 0;
+        long startTime = System.currentTimeMillis();
+        while (runDistance < DISTANCE) {
+            try {
+                int speed = (new Random()).nextInt(20);
+                runDistance += speed;
+                String log = "|";
+                int percentTravel = (runDistance * 100) / DISTANCE;
+                for (int i = 0; i < DISTANCE; i++) {
+                    if (percentTravel >= i + STEP) {
+                        log += "=";
+                    } else if (percentTravel >= i && percentTravel < i + STEP) {
+                        log += "o";
+                    } else {
+                        log += "-";
+                    }
+                }
+                log += "|";
+                System.out.println("Car" + this.name + ": " + log + " " + Math.min(DISTANCE, runDistance) + "KM");
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                System.out.println("Car " + this.name + " broke...");
+                break;
+            }
+        }
+        long endTime = System.currentTimeMillis();
+        System.out.println("Car" + this.name + " Finish in " + (endTime - startTime) / 1000 + "s");
+    }
+
+    public static void main(String[] args) {
+        Car carA = new Car("A");
+        Car carB = new Car("B");
+        Car carC = new Car("C");
+
+        Thread thread1 = new Thread(carA);
+        Thread thread2 = new Thread(carB);
+        Thread thread3 = new Thread(carC);
+
+        System.out.println("Distance: 100KM");
+        thread1.start();
+        thread2.start();
+        thread3.start();
+    }
+}
